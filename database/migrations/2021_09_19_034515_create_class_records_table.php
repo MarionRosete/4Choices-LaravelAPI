395e727030444c8719ea4136e4b9fc94b533c1db
Insider.Og <marionrosete1@gmail.com>
2021-09-19 11:49:33 +0800

    wrong username :D

--- database/migrations/2021_09_19_034515_create_class_records_table.php
+++ database/migrations/2021_09_19_034515_create_class_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateParticipantsTable extends Migration
+class CreateClassRecordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateParticipantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('participants', function (Blueprint $table) {
+        Schema::create('class_records', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateParticipantsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('participants');
+        Schema::dropIfExists('class_records');
     }
 }
